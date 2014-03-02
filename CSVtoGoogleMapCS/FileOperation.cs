08120251732d1b6b53aa42561a24597d36531510
kyufast <kyufast0711@gmail.com>
2014-03-03 03:31:01 +0900

    first commit
    first commit

---
+++ CSVtoGoogleMapCS/FileOperation.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPStoGoogleMap
+{
+    class FileOperation
+    {
+        public String InputFilePath { get; set; }
+        public String OutputDirctoryPath { get; set; }
+
+        public Boolean calcLogToGoogleMap()
+        {
+            csvToHTML();
+            csvToHTMLGraph();
+
+            return true;
+        }
+
+        
+
+        public Boolean csvToHTML()
+        {
+            String outputfile;
+            if (this.InputFilePath == null)
+            {
+                return false;
+            }
+            else
+            {
+                outputfile = getRawOutputFilePath()+"_output.html";
+            }          
+
+            System.IO.StreamReader sr = new
+              System.IO.StreamReader("..\\..\\gpssample.html");
+            System.IO.StreamReader srcsv = new System.IO.StreamReader(InputFilePath);
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(outputfile);
+
+            String str = sr.ReadLine();
+            //内容を一行ずつ読み込む
+            while (sr.Peek() > -1)
+            {
+                sw.WriteLine(str);
+                if (str.IndexOf("StartArray()") != -1)
+                {
+                    //ヘッダー行
+                    String strcsv = srcsv.ReadLine();
+                    //内容1行目
+                    strcsv = srcsv.ReadLine();
+                    long i = 0;
+                    while (strcsv != null)
+                    {
+                        String[] contents = strcsv.Split(',');
+                        sw.WriteLine("patharray[" + i
+                                + "] = new google.maps.LatLng(" + contents[1]
+                                + "," + contents[2] + ");");
+                        i++;
+                        strcsv = srcsv.ReadLine();
+                    }                    
+                }
+                str = sr.ReadLine();
+                //System.Diagnostics.Debug.WriteLine(sr.ReadLine());
+            }
+            //閉じる
+            sw.Close();
+            srcsv.Close();
+            sr.Close();
+            return true;
+
+
+        }
+
+        private Boolean csvToHTMLGraph()
+        {
+            String outputfile;
+            int pass=1;
+            if (this.InputFilePath == null)
+            {
+                return false;
+            }
+            else
+            {
+                outputfile = getRawOutputFilePath() + "_Graph.html";
+            }
+
+            System.IO.StreamReader sr = new
+              System.IO.StreamReader("..\\..\\Graph.html");
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(outputfile);
+
+
+            //行数によってある程度グラフのよみ飛ばし
+            {
+                System.IO.StreamReader srcsv = new System.IO.StreamReader(InputFilePath);
+                String strcsv = srcsv.ReadToEnd();
+                int csvline = strcsv.ToList().Where(c => c.Equals('\n')).Count();
+                srcsv.Close();
+
+                //長時間記録されてる場合は数を減らす
+                if (csvline > 1000)
+                {
+                    int i;
+                    for (i = 1; (csvline / i)>=1000; i++)
+                    {
+                    }
+                    pass = i;
+                }
+                else
+                {
+                    pass = 1;
+                }
+            }
+
+
+            String str = sr.ReadLine();
+            while (sr.Peek() > -1)
+            {
+                sw.WriteLine(str);
+                if (str.IndexOf("TimeInput") != -1)
+                {
+                    System.IO.StreamReader srcsv = new System.IO.StreamReader(InputFilePath);
+                    // ヘッダー行
+                    String strcsv = srcsv.ReadLine();
+                    // 内容1行目
+                    strcsv = srcsv.ReadLine();
+
+                    while (srcsv.Peek() > -1)
+                    {
+                        String[] contents = strcsv.Split(',');
+                        sw.WriteLine("<th scope=\"col\">" + contents[0] + "</th>");
+                        //長時間記録されてる場合は数を減らす
+                        for (int i = 0; i < pass; i++)
+                        {
+                            strcsv = srcsv.ReadLine();
+                        }
+                    }
+                    srcsv.Close();
+                }
+
+                if (str.IndexOf("SpeedInput") != -1)
+                {
+                    System.IO.StreamReader srcsv = new System.IO.StreamReader(InputFilePath);
+                    // ヘッダー行
+                    String strcsv = srcsv.ReadLine();
+                    // 内容1行目
+                    strcsv = srcsv.ReadLine();
+
+                    while (srcsv.Peek() > -1)
+                    {
+                        String[] contents = strcsv.Split(',');
+                        sw.WriteLine("<td>" + contents[6] + "</td>");
+                        //長時間記録されてる場合は数を減らす
+                        for (int i = 0; i < pass; i++)
+                        {
+                            strcsv = srcsv.ReadLine();
+                        }
+                    }
+                    srcsv.Close();
+                }
+                str = sr.ReadLine();
+
+            }
+            sw.Close();
+            sr.Close();
+
+            return true;
+
+        }
+
+        private String getRawOutputFilePath()
+        {
+            String outputfile;
+            if (this.OutputDirctoryPath == null)
+            {
+                outputfile = this.InputFilePath.Substring(0, this.InputFilePath.LastIndexOf('.'));
+            }
+            else
+            {
+                int start = this.InputFilePath.LastIndexOf("\\") + 1;
+                int end = this.InputFilePath.LastIndexOf('.') - start;
+                outputfile = OutputDirctoryPath + "\\" + InputFilePath.Substring(start, end);
+            }
+            return outputfile;
+        }
+    }
+}
---
+++ CSVtoGoogleMapCS/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GPStoGoogleMap
+{
+    public partial class Form1 : Form
+    {
+        Label inputlabel, outputlabel;
+        Button inputbutton, outputbutton, okbutton;
+        FileOperation fileoperation;
+        OpenFileDialog inputFileDialog;
+        FolderBrowserDialog outputDirctory;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            init();
+
+            this.inputlabel = new Label
+            {
+                Location = new Point(10, 20),
+                AutoSize = true,
+                Text = "入力ファイル名",
+            };
+            this.Controls.Add(inputlabel);
+
+            this.inputbutton = new Button();
+            this.inputbutton.Location = new Point(10, 50);
+            this.inputbutton.Size = new Size(170, 30);
+            this.inputbutton.Text = "入力ファイル";
+            this.inputbutton.Click += new EventHandler(this.inputButtonClick);
+            this.Controls.Add(this.inputbutton);
+
+
+            this.outputlabel = new Label
+            {
+                Location = new Point(10, 100),
+                AutoSize = true,
+                Text = "出力フォルダは選択しない時入力ファイルのフォルダです",
+            };
+            this.Controls.Add(outputlabel);
+
+            this.outputbutton = new Button();
+            this.outputbutton.Location = new Point(10, 140);
+            this.outputbutton.Size = new Size(170, 30);
+            this.outputbutton.Text = "出力ファイル";
+            this.outputbutton.Click += new EventHandler(this.ouputButtonClick);
+            this.Controls.Add(this.outputbutton);
+
+            this.okbutton = new Button();
+            this.okbutton.Location = new Point(10, 190);
+            this.okbutton.Size = new Size(170, 30);
+            this.okbutton.Text = "実行";
+            this.okbutton.Click += new EventHandler(this.okButtonClick);
+            this.Controls.Add(this.okbutton);
+
+        }
+
+        void init()
+        {
+            fileoperation = new FileOperation();
+        }
+        void inputButtonClick(object sender, EventArgs e)
+        {
+            inputFileDialog = new OpenFileDialog();
+            if (inputFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileoperation.InputFilePath=inputFileDialog.FileName;
+                Debug.WriteLine("input Path:" + inputFileDialog.FileName);
+                inputlabel.Text = inputFileDialog.FileName;
+                //System.IO.StreamReader sr = new
+                //  System.IO.StreamReader(inputFileDialog.FileName);
+                //MessageBox.Show(sr.ReadToEnd());
+                //sr.Close();
+            }
+        }
+
+        void ouputButtonClick(object sender, EventArgs e)
+        {
+            outputDirctory = new FolderBrowserDialog();
+            if (outputDirctory.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                Debug.WriteLine("output Path:" + outputDirctory.SelectedPath);
+                fileoperation.OutputDirctoryPath = outputDirctory.SelectedPath;
+                outputlabel.Text = outputDirctory.SelectedPath;
+            }
+        }
+
+        void okButtonClick(object sender, EventArgs e)
+        {
+            fileoperation.calcLogToGoogleMap();
+        }
+    }
+}
